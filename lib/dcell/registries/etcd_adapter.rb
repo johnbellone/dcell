966cdb8b6113546c907f74487c06e024caa01f11
Hendrik Beskow <hendrik-github-mac@beskow.de>
2014-03-01 16:03:54 +0100

    uses uris to configure etcd servers

--- lib/dcell/registries/etcd_adapter.rb
+++ lib/dcell/registries/etcd_adapter.rb
@@ -1,18 +1,20 @@
+require 'uri'
 require 'etcd'
 
 module DCell
   module Registry
     class EtcdAdapter
       PREFIX = '/dcell'
-      DEFAULT_PORT = 4001
-      
+      DEFAULT_SCHEME = 'http'
+      DEFAULT_PORT   = 4001
+
       # Create a new connect to Etc daemon.
       #
       # servers: List of Etcd servers to connect to. Each server
       # has a host/port configuration.
       def initialize(options={})
         options = options.inject({}) { |h,(k,v)| h[k.to_s] = v; h }
-        
+
         @env = options['env'] || 'production'
         @base_path = "#{PREFIX}/#{@env}"
 
@@ -27,15 +29,21 @@ module DCell
 
         # Add default Etcd port unless specified.
         servers.map! do |server|
-          if server[/:\d+$/]
+          server = server.is_a?(URI) ? server : URI(server)
+          if server.scheme && server.host && server.port
             server
           else
-            "#{server}:#{DEFAULT_PORT}"
+            server = URI("#{DEFAULT_SCHEME}://#{server.to_s}:#{DEFAULT_PORT}")
           end
         end
-        
-        server = server.first
-        @etcd = Etcd.client(host: server.host, port: server.port)
+
+        server = servers[rand(servers.count)]
+        @etcd = Etcd.client(host: server.host,
+          port: server.port,
+          use_ssl: server.scheme == 'https' ? true : false,
+          user_name: server.user,
+          password: server.password)
+
         @node_registry = Registry.new(@etcd, @base_path, :nodes)
         @global_registry = Registry.new(@etcd, @base_path, :globals)
       end
@@ -48,26 +56,28 @@ module DCell
       def get_global(key);        @global_registry.get(key) end
       def set_global(key, value); @global_registry.set(key, value) end
       def global_keys;            @global_registry.all end
-      def clear_globals;          @global_registry.clear end      
+      def clear_globals;          @global_registry.clear end
 
       class Registry
         def initialize(etcd, base_path, name)
           @etcd = etcd
           @base_path = File.join(base_path, name.to_s)
-          @etcd.create(@base_path, dir: true)
+          unless @etcd.exists?(@base_path)
+            @etcd.create(@base_path, dir: true)
+          end
         end
 
         def get(key)
           result = @etcd.get(File.join(base_path, key))
-          Marshal.load(result.value) 
+          Marshal.load(result.value) if result
         end
 
         def set(key, value)
-          path = File.join base_path, key
-          string = Marshal.dump value
+          path = File.join @base_path, key
+          string = Marshal.dump(value)
           @etcd.set(path, value: string)
         rescue Etcd::NodeExist
-          @etcd.create(path, value: string)
+          @etcd.update(path, value: string)
         end
 
         def all
